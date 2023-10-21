6d5eee4563fcc09ecdad12030a7195ab28dcb515
Kallinteris Andreas <30759571+Kallinteris-Andreas@users.noreply.github.com>
2023-10-21 08:34:08 +0000

    organize swimmer

--- swimmer/verify.sh
+++ swimmer/verify.sh
@@ -22,3 +22,6 @@ pip install "mujoco==2.3.7"
 python verify_env_match.py
 pip install "mujoco-py==2.1.2.14"
 python verify_env_match_old.py
+
+
+#note : this repo gor gymnasium as used: https://github.com/Kallinteris-Andreas/Gymnasium-kalli/tree/768b0d1072d3748f3c403e0995ba425d036b287d
